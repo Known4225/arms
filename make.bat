e92e24e60f7b4abf77af64a900d16fea51056cdd
Ryan Srichai <ryansrichai@gmail.com>
2023-07-03 09:11:51 -0500

    genericUpdate

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-gcc arms.c -L./Windows -lglfw3 -lopengl32 -lgdi32 -lglad -lole32 -luuid -O3 -o arms.exe -Wall
+gcc arms.c -L./Windows -lglfw3 -lopengl32 -lgdi32 -lglad -lole32 -luuid -O3 -o arms.exe
